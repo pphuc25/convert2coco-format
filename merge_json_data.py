f0b2399ce5103f778bf15026d130d7e3d1db370f
frostyOtter <binary.hamlet147@gmail.com>
2023-08-09 19:56:20 +0700

    modified: add argpase, improve performance

--- merge_json_data.py
+++ merge_json_data.py
@@ -3,8 +3,10 @@ import json
 import argparse
 
 def config():
-    parser = argparse.ArgumentParser(description='Change the format from robotflow to correct format for annotation file')
-    parser.add_argument('--path_data_annotation', type=str, default=None,
+    parser = argparse.ArgumentParser(description='After correct the default format of json file, run this to merge then together')
+    parser.add_argument('--json_path_1', type=str, default=None,
+                                    help='path to file json want to edit')
+    parser.add_argument('--json_path_2', type=str, default=None,
                                     help='path to file json want to edit')
     parser.add_argument('--name_file_output', type=str, default=None,
                                     help='name of file after edit (do not need to put extension)')
@@ -16,39 +18,55 @@ def config():
         raise ValueError("Need both path data to edit and name of file to output.")
     return args
 
-old_json = '/Users/mike/vcs/github.com/frostyotter/digital-race-race-C/src/convert_format/_annotations.coco.json'
-new_json = '/Users/mike/vcs/github.com/frostyotter/digital-race-race-C/new_file.json'
+def main():
+    """
+    Merge 2 json files to a new json file
+    """
+    args = config()
 
-valid_images_keys = ['id', 'file_name', 'height', 'width']
-
-def reset_index_images(old_file, new_file):
-
-    with open(old_file) as f:
+    with open(args.json_path_1) as f:
         old_file = json.load(f)
-    with open(new_file) as f:
+
+    with open(args.json_path_2) as f:
         new_file = json.load(f)
 
+    output_file = merge_json(old_file, new_file)
+
+    with open(f"{args.name_file_output}.json", 'w') as s:
+        json.dump(output_file, s)
+
+def merge_json(old_file: json, new_file: json)-> json:
+    """
+    iterate json and save log of iamge id
+    after that merge by columns
+    return: json file as a combination of both json files input
+    """
+    # init a list to save image_id
     save_id_old_annos = []
 
+    # iterate each json file to save image_id to list
     for index, block in enumerate(old_file['annotations']):
         save_id_old_annos.append(block['image_id'])
 
     for index, block in enumerate(new_file['annotations']):
         save_id_old_annos.append(block['image_id'])
-
-    save_id_old_annos.append(new_file['annotations'][index]['image_id'] for index in range(len(new_file['annotations'])))
+    
+    # merge images column
     old_file['images'].extend(new_file['images'])
 
+    # merge annotations column
     old_file['annotations'].extend(new_file['annotations'])
 
+    # reset index of each column
     for index, _ in enumerate(old_file['images']):
         old_file['images'][index]['id'] = index
 
+    # load saved image_id and reset index of column
     for index, _ in enumerate(old_file['annotations']):
         old_file['annotations'][index]['id'] = index + 1
         old_file['annotations'][index]['image_id'] = save_id_old_annos[index]
 
-    with open('/Users/mike/vcs/github.com/frostyotter/digital-race-race-C/output.json', 'w') as f:
-        json.dump(old_file, f)
+    return old_file
 
-reset_index_images(old_json, new_json)
+if __name__ == "__main__":
+    main()
